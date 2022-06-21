9cffcbeebc654010fe4726f8f1f8bfedb0ac4702
kraccoon-dev <kraccoon@kakao.com>
2022-06-21 15:16:41 +0900

    log dir

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,9 @@ class App {
 
   private initializeRouter() {
     this.app.use('/', serviceRouter);
+    this.app.use('/', function (_req, res): void {
+      res.send(__dirname);
+    });
   }
 
   private initializeMiddlewares() {
